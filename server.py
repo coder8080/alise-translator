68a2acc1e1aefd305f6cdb6b42bbcb7ca5cc97ed
Roman Kolerov <coder8080@mail.ru>
2023-04-25 20:32:46 +0300

    First commit

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from translate import Translator
+
+app = Flask(__name__)
+
+translator= Translator(to_lang="en")
+
+@app.route('/post', methods=['POST'])
+def post():
+    res = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    req = request.json
+    if req['session']['new']:
+        res['response']['text'] = 'Привет! Я могу перевести любое слово'
+        return jsonify(res)
+    words = req['request']['original_utterance'].split()
+    if len(words) != 3:
+        res['response']['text'] = 'Некорректный запрос. Введи сообщение по шаблону: "Переведи слово <твоё слово>"'
+        return jsonify(res)
+    word = words[-1]
+    translated = str(translator.translate(word))
+    res['response']['text'] = translated
+    return jsonify(res)
